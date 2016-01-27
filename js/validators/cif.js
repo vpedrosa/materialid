5fe3707a63243cf49d9096b34537a34a1e00e0bb
Valentin <burflip@gmail.com>
2016-01-27 13:57:26 +0100

    Bugfix on CIF validator

--- js/validators/cif.js
+++ js/validators/cif.js
@@ -8,40 +8,46 @@ function cif(field, settings) {
     var is_valid = false;
     if (notEmpty(field)) {
         var value = field.val().toUpperCase();
-        is_valid = validateCIF(cif)
+        is_valid = validateCIF(value)
     } else {
         is_valid = false;
     }
     return is_valid;
 }
 
-function validateCIF(cif){
-    var value = cif;
-    var suma, ultima, unumero, xxx;
-    var is_valid = false, pares = 0, impares = 0;
-    var uletra = ["J", "A", "B", "C", "D", "E", "F", "G", "H", "I"];
-    var regular = /^[ABCDEFGHKLMNPQS]\d{7}[0-9,A-J]$/g;
-    if (regular.test(value)) {
-        ultima = value.substr(8, 1);
+function validateCIF(texto)
+{
+    var pares = 0;
+    var impares = 0;
+    var suma;
+    var ultima;
+    var unumero;
+    var uletra = new Array("J", "A", "B", "C", "D", "E", "F", "G", "H", "I");
+    var xxx;
 
-        for (var cont = 1; cont < 7; cont++) {
-            xxx = (2 * parseInt(value.substr(cont++, 1))).toString() + "0";
-            impares += parseInt(xxx.substr(0, 1)) + parseInt(xxx.substr(1, 1));
-            pares += parseInt(value.substr(cont, 1));
-        }
-        xxx = (2 * parseInt(value.substr(cont, 1))).toString() + "0";
-        impares += parseInt(xxx.substr(0, 1)) + parseInt(xxx.substr(1, 1));
+    texto = texto.toUpperCase();
 
-        suma = (pares + impares).toString();
-        unumero = parseInt(suma.substr(suma.length - 1, 1));
-        unumero = (10 - unumero).toString();
-        if (unumero == 10) unumero = 0;
+    var regular = /^[ABCDEFGHKLMNPQS]\d\d\d\d\d\d\d[0-9,A-J]$/g;
+    if (!regular.exec(texto)) return false;
 
-        if ((ultima == unumero) || (ultima == uletra[unumero]))
-            is_valid = true;
-        else {
-            is_valid = false;
-        }
+    ultima = texto.substr(8,1);
+
+    for (var cont = 1 ; cont < 7 ; cont ++){
+        xxx = (2 * parseInt(texto.substr(cont++,1))).toString() + "0";
+        impares += parseInt(xxx.substr(0,1)) + parseInt(xxx.substr(1,1));
+        pares += parseInt(texto.substr(cont,1));
     }
-    return is_valid;
+    xxx = (2 * parseInt(texto.substr(cont,1))).toString() + "0";
+    impares += parseInt(xxx.substr(0,1)) + parseInt(xxx.substr(1,1));
+
+    suma = (pares + impares).toString();
+    unumero = parseInt(suma.substr(suma.length - 1, 1));
+    unumero = (10 - unumero).toString();
+    if(unumero == 10) unumero = 0;
+
+    if ((ultima == unumero) ||  (ultima == uletra[unumero]))
+        return true;
+    else
+        return false;
+
 }
